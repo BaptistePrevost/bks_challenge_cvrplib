5e016ea0939bbbf0f327e755f872f585a57c3aa1
Baptiste Prévost <baptiste_prevost@hotmail.com>
2025-09-02 22:45:13 +0200

    Introduce maxPopulationSize

--- solver/include/parameters.hpp
+++ solver/include/parameters.hpp
@@ -3,12 +3,14 @@
 class Parameters {
 
     public:
-    Parameters(int populationSize, int maxNbRoutes);
+    Parameters(int populationSize, int maxPopulationSize, int maxNbRoutes);
 
     const int populationSize() const;
+    const int maxPopulationSize() const;
     const int maxNbRoutes() const;
 
     private:
     int populationSize_;
+    int maxPopulationSize_;
     int maxNbRoutes_;
 };
--- solver/src/parameters.cpp
+++ solver/src/parameters.cpp
@@ -1,10 +1,16 @@
 #include "parameters.hpp"
 
-Parameters::Parameters(int populationSize, int maxNbRoutes) : 
-        populationSize_(populationSize), maxNbRoutes_(maxNbRoutes) {}
+Parameters::Parameters(int populationSize, int maxPopulationSize, int maxNbRoutes) : 
+        populationSize_(populationSize), 
+        maxPopulationSize_(maxPopulationSize), 
+        maxNbRoutes_(maxNbRoutes) {}
 
 const int Parameters::populationSize() const {
     return populationSize_;
+}
+
+const int Parameters::maxPopulationSize() const {
+    return maxPopulationSize_;
 }
 
 const int Parameters::maxNbRoutes() const {
